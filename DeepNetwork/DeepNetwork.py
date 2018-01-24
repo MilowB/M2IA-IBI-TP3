165055aeeecfe4c4fde94437c0ab1249d9f3be79
AUBRET ARTHUR p1205418 <arthur.aubret@etu.univ-lyon1.fr>
2018-01-24 15:23:15 +0100

    deep learning debug

--- DeepNetwork/DeepNetwork.py
+++ DeepNetwork/DeepNetwork.py
@@ -10,30 +10,39 @@ dtype = torch.FloatTensor
 # dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
 
 
-def deepNetwork(sizeEntries, sizeOut, sizeHidden, data, test_data, test_data_label):
-    # A mon avis le truc qui ne va pas vient du paramétrage que l'on fait sur w1 et w2
+def deepNetwork(sizeEntries, sizeOut, sizeHidden, data, data_label, test_data, test_data_label):
+    # A mon avis le truc qui ne va pas vient du parametrage que l'on fait sur w1 et w2
 
     # N is batch size; D_in is input dimension;
     # H is hidden dimension; D_out is output dimension.
-    N, D_in, H, D_out = len(data), sizeEntries, 5, sizeOut
+    D_in, H, D_out = sizeEntries, sizeHidden, sizeOut
 
-    # Entrée
-    x = Variable(data.type(dtype), requires_grad=False)
-    # Prédiction
-    y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+    # Entree
+    entries = Variable(data.type(dtype), requires_grad=False)
+    # Prediction
+    labels = Variable(data_label.type(dtype), requires_grad=False)
+    #y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
 
-    # Neurones et leurs poids (w1 couche d'entrée, w2 couche de sortie)
-    w1 = Variable(torch.randn(D_in, N/50).uniform_(-0.1,0.1).type(dtype), requires_grad=True) # Couche entrée
-    w2 = Variable(torch.randn(N/50, D_out).uniform_(-0.1,0.1).type(dtype), requires_grad=True) # Couche sortie
+    # Neurones et leurs poids (w1 couche d entree, w2 couche de sortie)
+    w1 = Variable(torch.randn(D_in, sizeHidden).type(dtype), requires_grad=True) # Couche entree
+    w2 = Variable(torch.randn(sizeHidden, D_out).type(dtype), requires_grad=True) # Couche sortie
 
     #epsilon
-    learning_rate = 1e-5
+    learning_rate = 5e-4
 
-    for t in range(500):
+    for t in range(40000):
+        x = Variable(data[t].type(dtype), requires_grad=False)
+        y = Variable(data_label[t].type(dtype), requires_grad=False)
+        #x = entries[t]
+        #y= labels[t]
+
+        x = x.unsqueeze(0)
+        #x.mm scalair, clam fonction relu, mm scalaire sur la sortie
         y_pred = x.mm(w1).clamp(min=0).mm(w2)
 
+        # erreur
         loss = (y_pred - y).pow(2).sum()
-        print(t, loss.data[0])
+        #print(t, loss.data[0])
         loss.backward()
 
         w1.data -= learning_rate * w1.grad.data
@@ -43,23 +52,22 @@ def deepNetwork(sizeEntries, sizeOut, sizeHidden, data, test_data, test_data_lab
         w2.grad.data.zero_()
 
 
-    # Traitement du test pour voir la performance du réseau
+    # Traitement du test pour voir la performance du reseau
     accurrancy= 0
-
-    x = Variable(test_data , requires_grad=False)
-    y = Variable(test_data_label, requires_grad=False)
-
-    # Calcul de la matrice de prédiction avec les poids modifiés plus haut
-    # 1 tableau de prédiction de taille 10 par ligne du tableau de test
-    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+    # Calcul de la matrice de prediction avec les poids modifies plus haut
+    # 1 tableau de prediction de taille 10 par ligne du tableau de test
 
     for i in range(len(test_data)):
-        # Récupération de la prédiction de la ligne i du test
-        d = y_pred[i,:]
-        # Valeur max afin d'identifier la valeur prédite (indice où se trouve le 1)
-        valuesx, indicesx = torch.max(d, 0)
-        indices2 = numpy.argmax(test_data_label[i, :])
-        indices1 =  indicesx.data.numpy()[0]
+        # Recuperation de la prediction de la ligne i du test
+        x = Variable(test_data[i].type(dtype), requires_grad=False)
+        x = x.unsqueeze(0)
+        y_pred = x.mm(w1).clamp(min=0).mm(w2)
+
+        # Valeur max afin d identifier la valeur predite (indice ou se trouve le 1)
+        valuesx, indicesx = y_pred.t().max(0)
+        indices2 = numpy.argmax(test_data_label[i])
+        indices1 =  indicesx.data[0]
+        #print indices2, indices1
         #print("predicted %f label %f" % (indices1,indices2  ))
         if (indices1==indices2):
             accurrancy += 1
--- DeepNetwork/main.py
+++ DeepNetwork/main.py
@@ -10,9 +10,9 @@ def main():
     data = cPickle.load(gzip.open('../mnist.pkl.gz'))
     apprentissage = data[0]
     test = data[1]
-    
-    sizeIn = len(data[0][0][0])
-    hiddenlayer = 100
+
+    hiddenlayer = 10
+    sizeIn = len(apprentissage[0][0])
     sizeOut = len(apprentissage[1][0])
     #e= 0.0001
     e = 0.1
@@ -22,25 +22,14 @@ def main():
     # labels de la base de test
     test_data_label = torch.Tensor(data[1][1])
 
-    network = deepNetwork(sizeIn, sizeOut, hiddenlayer, torch.from_numpy(apprentissage[0]), test_data, test_data_label)
+    network = deepNetwork(sizeIn,
+                          sizeOut,
+                          hiddenlayer,
+                          torch.from_numpy(apprentissage[0]),
+                          torch.from_numpy(apprentissage[1]),
+                          test_data,
+                          test_data_label)
 
-    '''
-    total = 0
-    gagne = 0
-    for x in range(10000):
-        d =  x % len(apprentissage[0])
-        data = torch.from_numpy(apprentissage[0][d])
-        label = apprentissage[1][d]
-        network.activity(data, label)
-
-    for x in range(len(test[0])):
-        data = torch.from_numpy(test[0][x])
-        label = test[1][x]
-        if int(label[network.predict(data,label)]) == 1:
-            gagne += 1
-        total += 1
-    print (float(gagne) / float(total)) * 100, "%"
-    '''
 
 if __name__ == '__main__':
     main()
