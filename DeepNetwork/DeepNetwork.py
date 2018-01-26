bda5b87647b8e8b773065ccf437e76c9cfdb55e4
mickael <mickael.bettinelli@etu.uni-lyon1.fr>
2018-01-26 20:44:58 +0100

    biais ajouter au deep

--- DeepNetwork/DeepNetwork.py
+++ DeepNetwork/DeepNetwork.py
@@ -17,17 +17,32 @@ class DeepNetwork:
         # D_out is output dimension.
         D_in, D_out = sizeEntries, sizeOut
         resize = 0.13
-        # Neurones et leurs poids (w1 couche d'entree, w2 couche de sortie)
-        self.wentry = Variable(torch.randn(D_in, dimensions[0]).uniform_(-resize, resize).type(dtype), requires_grad=True) # Couche entree
+
+        '''
+        # Ajout du biais sur la premiere couche
+        x = torch.ones(len(wentry))
+        wentry = torch.cat((wentry, x), 1)
+        '''
+        wentry = torch.randn(D_in + 1, dimensions[0]).uniform_(-resize, resize).type(dtype)
+
+        # Neurones et leurs poids
+        self.wentry = Variable(wentry, requires_grad=True) # Couche entree
         
+
         self.whidden = []
         lastSize = dimensions[0]
         for i in range(0, len(dimensions)):
-            layer = Variable(torch.randn(lastSize, dimensions[i]).uniform_(-resize, resize).type(dtype), requires_grad=True) # Couche cachee
+            '''
+            # Ajout du biais sur les couches cachees
+            x = torch.ones(len(hiddenLayer))
+            hiddenLayer = torch.cat((hiddenLayer, x), 1)
+            '''
+            hiddenLayer = torch.randn(lastSize + 1, dimensions[i]).uniform_(-resize, resize).type(dtype)
+            layer = Variable(hiddenLayer, requires_grad=True) # Couche cachee
             self.whidden.append(layer)
             lastSize = dimensions[i]
 
-        self.wout = Variable(torch.randn(lastSize, D_out).uniform_(-resize, resize).type(dtype), requires_grad=True) # Couche sortie
+        self.wout = Variable(torch.randn(lastSize + 1, D_out).uniform_(-resize, resize).type(dtype), requires_grad=True) # Couche sortie
         self.learning_rate = e
 
         if self.debug:
@@ -38,17 +53,27 @@ class DeepNetwork:
 
     def train(self, data, label):
         x = Variable(data.type(dtype), requires_grad=False)
+        x = self._addBiais(x)
         y = Variable(label.type(dtype), requires_grad=False)
 
         for t in range(100):
+            lastLayer = self.wentry
             y_pred = x.mm(self.wentry).clamp(min=0)
+
+            y_pred = self._addBiais(y_pred)
+            
             for layer in self.whidden:
                 y_pred = y_pred.mm(layer)
-            y_pred = y_pred.mm(self.wout)
+                lastLayer = layer
+                y_pred = self._addBiais(y_pred)
 
+
+            y_pred = y_pred.mm(self.wout)            
             loss = (y_pred - y).pow(2).sum()
+
             if self.debug:
                 print(t, loss.data[0])
+
             loss.backward()
             
             self.wentry.data -= self.learning_rate * self.wentry.grad.data
@@ -66,13 +91,17 @@ class DeepNetwork:
         accurrancy= 0
 
         x = Variable(test_data , requires_grad=False)
+        x = self._addBiais(x)
         y = Variable(test_data_label, requires_grad=False)
-
+        
         # Calcul de la matrice de prediction avec les poids modifies plus haut
         # 1 tableau de prediction de taille 10 par ligne du tableau de test
         y_pred = x.mm(self.wentry).clamp(min=0)
+        y_pred = self._addBiais(y_pred)
         for layer in self.whidden:
             y_pred = y_pred.mm(layer)
+            y_pred = self._addBiais(y_pred)
+
         y_pred = y_pred.mm(self.wout)
 
         for i in range(len(test_data)):
@@ -87,3 +116,8 @@ class DeepNetwork:
         print("Valeurs bien predit: %d " % (accurrancy))
         print("Valeurs mal predit:  %d " % (len(test_data) - accurrancy))
         print("Taux de reussite:    %f " % ((float(accurrancy)/len(test_data)) * 100))
+
+    def _addBiais(self, matrix):
+        # Ajout du biais sur les nouvelles donnees
+        b = Variable(torch.ones(len(matrix), 1), requires_grad=False)
+        return torch.cat((matrix, b), 1)
--- DeepNetwork/main.py
+++ DeepNetwork/main.py
@@ -14,16 +14,15 @@ def main():
     sizeIn = len(data[0][0][0])
     hiddenlayer = 100
     sizeOut = len(apprentissage[1][0])
-    #e= 0.0001
-    e = 0.1
+    e = 5e-6
 
     # images de la base de test
     test_data = torch.Tensor(data[1][0])
     # labels de la base de test
     test_data_label = torch.Tensor(data[1][1])
 
-    #Dimension des couches cachees
-    network = DeepNetwork([10], sizeIn, sizeOut, 5e-6, False)
+    #Dimension des couches cachees [10, 10]
+    network = DeepNetwork([100, 10, 100], sizeIn, sizeOut, e, True)
     network.train(torch.from_numpy(apprentissage[0]), torch.from_numpy(apprentissage[1]))
     network.test(test_data, test_data_label)
 
